95f77819c090b7724cb67718246d0c08b3c7aa65
gokatz <gokulkathirvel@live.com>
2020-01-22 22:53:21 +0530

    code intend

--- app/services/store.js
+++ app/services/store.js
@@ -8,7 +8,7 @@ export default class StoreService extends Service {
     // rootUrl = 'http://varun-3902:8080';
     rootUrl = 'http://localhost:8080';
     rootUrl = 'http://justnote.westus2.cloudapp.azure.com';
-rootUrl = 'https://techprojects.xyz:8443';
+    rootUrl = 'https://techprojects.xyz:8443';
     let { method = 'get', data = {}, params = {} } = options;
     url = rootUrl + url;
 
